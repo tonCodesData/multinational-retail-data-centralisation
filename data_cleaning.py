e4b674e4c38ee4ace1c4ee87fa75c16e9bac61f7
Tonmoy <rashid920129@gmail.com>
2023-03-03 02:31:20 +0000

    restarts writing

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,4 @@
 class DataCleaning:
     # clean data of each data sources
-    def clean_user_data(self, df):
-        # perform data cleaning operations
-        cleaned_df = df.dropna() # example: drop rows with null values
-        return cleaned_df
+
     

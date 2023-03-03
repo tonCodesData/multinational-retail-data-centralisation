272a18adcbfe9f7bfdb55e464cf962c239e6ea1d
Tonmoy <rashid920129@gmail.com>
2023-03-03 03:21:23 +0000

    returns the table

--- data_extraction.py
+++ data_extraction.py
@@ -6,3 +6,13 @@ class DataExtractor:
     # that help extract data from different data sources.
     # The methods contained will be fit to extract data from a particular data source, 
     # these sources will include CSV files, an API and an S3 bucket.
+
+    #step 5: Develop a method called read_rds_table 
+    # which will extract the database table to a pandas DataFrame.
+    # take in an instance of DatabaseConnector class 
+    # and the table name as an argument and return a pandas Df
+    def read_rds_table(self, table_name, db_conn):
+        engine = db_conn.init_db_engine()
+        users = pd.read_sql_query(f"select * from {table_name}", engine)
+        return users
+

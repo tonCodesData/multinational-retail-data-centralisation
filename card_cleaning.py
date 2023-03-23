ec93994d2bc01401b45691de57b5acb80435b24c
Tonmoy <rashid920129@gmail.com>
2023-03-23 17:01:41 +0000

    cleanse card data

--- card_cleaning.py
+++ card_cleaning.py
@@ -6,16 +6,70 @@ import boto3
 from database_utils import DatabaseConnector
 from data_cleaning import DataCleaning
 from data_extraction import DataExtractor
+import datetime as dt
+import numpy as np
 
 # %%
-data_extractor = DataExtractor()
+df = pd.read_csv('card.csv')
 # %%
-card_df = data_extractor.retrieve_pdf_data("https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf")
+pd.set_option('display.max_rows', 1000) 
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
+# %%
+# %%
 
+# %% drop null values
+df.dropna(inplace=True)
+
+
+# %% work with card_number 
+df['card_number'] = df['card_number'].astype('string')
+
+
+# %% clean card_provider
+df['card_provider'] = df['card_provider'].astype('string')
 #%%
-type(card_df)
+card_prov_ls = ['VISA 16 digit', 'JCB 16 digit', 'VISA 13 digit', 'JCB 15 digit', 'VISA 19 digit', 'Diners Club / Carte Blanche', 'American Express', 'Maestro', 'Discover', 'Mastercard']
+#%%
+df['valid_provider'] = df['card_provider'].apply(lambda x: 'Yes' if x in card_prov_ls else 'No')
+#%%
+df = df[df['valid_provider']=='Yes']
+#%%
+df['card_provider'] = df['card_provider'].astype('category')
+#%%
+df.drop(['valid_provider'], axis=1, inplace=True) 
+
+
+
+# %% clean date_payment_confirmed
+set(df['date_payment_confirmed'])
 # %%
-card_df
+df['date_payment_confirmed'] # just transform into datetime
 # %%
-card_df.to_csv('card.csv')
+df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], errors='coerce', infer_datetime_format=True)
 # %%
+
+
+# %% work with expiry date
+set(df['expiry_date']) #some values need to be dropped which are not dates but texts and occurs only once
+# %%
+df['expiry_date'].value_counts()
+# %%
+df['dt'] = pd.to_datetime(df['expiry_date'], format="%m/%y")
+#%%
+df['dt']
+# %%
+# df['expiry_date'] = df['dt'].map(lambda x: x.strftime('%m/%y'))
+# %%
+df['expiry_date'] = pd.to_datetime(df['dt']).dt.to_period('M')
+# %%
+df['expiry_date']
+# %%
+df.drop(['dt'], axis=1, inplace=True)
+
+
+
+# %% working with Unnamed:0 
+df.rename(columns={'Unnamed: 0': 'groups'}, inplace=True)
+# %%
+df.info()
--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class DataCleaning:
     # clean data of each data sources
     def __init__(self) -> None:
@@ -5,13 +7,43 @@ class DataCleaning:
     def clean_user_data(self, df): 
         # clean user data
         # look for 
+        
+        # drops the column 'Unnamed:0' which seems to be unnecessary
+        # df.drop(['Unnamed: 0'], axis=1, inplace=True)
+        # sort_values
+        df.sort_values(by=['index'], inplace=True)
+
         # NULL values, 
-        cleaned_df = df.dropna()
-        # errors in dates, 
+        # errors in dates
+        df['date_of_birth'] = pd.to_datetime(df['date_of_birth'], errors='coerce', infer_datetime_format=True)
+        df['join_date'] = pd.to_datetime(df['join_date'], errors='coerce', infer_datetime_format=True)
         # incorrectly typed values
+
+        ## make all names titled
+        df['last_name'] = df['last_name'].astype('string')
+        df['last_name'] = df['last_name'].apply(lambda x: x.title())
         # rows filled with wrong information
-        return cleaned_df
+        
+        # replace 'GGB' with 'GB'
+        # drop other wrong ones
+        df['country_code'] = df['country_code'].replace('GGB','GB')
+        mask = (df.country_code=='GB') | (df.country_code=='DE') | (df.country_code=='US') 
+        new = df[mask]
+
+        # validating and standardizing phone number
+        new['phone_number'] = new['phone_number'].astype('string')
+        new['phone_number'] = new['phone_number'].str.replace(' ', '')
+        new['phone_number'] = new['phone_number'].str.replace('\(', '')
+        new['phone_number'] = new['phone_number'].str.replace('\)', '')
+        new['phone_number'] = new['phone_number'].str.replace('.', '')
+        new['phone_number'] = new['phone_number'].str.replace('+', '00')
+        new['phone_number'] = new['phone_number'].str.replace('-', '')
+
+        # standardizing address
+        df = new
+        return df                                                                                       
     
+
     #hod called clean_card_data in your DataCleaning class 
     # to clean the data to remove any erroneous values, 
     # NULL values or errors with formatting.

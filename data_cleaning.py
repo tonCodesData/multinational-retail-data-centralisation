fea497a055372dd3c908ec60b11935945f6c63c8
Tonmoy <rashid920129@gmail.com>
2023-03-23 17:20:28 +0000

    updates clean_card_data()

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,20 +4,12 @@ class DataCleaning:
     # clean data of each data sources
     def __init__(self) -> None:
         pass
-    def clean_user_data(self, df): 
-        # clean user data
-        # look for 
-        
-        # drops the column 'Unnamed:0' which seems to be unnecessary
-        # df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    def clean_user_data(self, df):         
         # sort_values
         df.sort_values(by=['index'], inplace=True)
 
-        # NULL values, 
-        # errors in dates
         df['date_of_birth'] = pd.to_datetime(df['date_of_birth'], errors='coerce', infer_datetime_format=True)
         df['join_date'] = pd.to_datetime(df['join_date'], errors='coerce', infer_datetime_format=True)
-        # incorrectly typed values
 
         ## make all names titled
         df['last_name'] = df['last_name'].astype('string')
@@ -44,62 +36,34 @@ class DataCleaning:
         return df                                                                                       
     
 
-    #hod called clean_card_data in your DataCleaning class 
+    # hod called clean_card_data in your DataCleaning class 
     # to clean the data to remove any erroneous values, 
     # NULL values or errors with formatting.
     def clean_card_data(self, df): 
-        # %% drop null values
+        # drop null values
         df.dropna(inplace=True)
 
-
-        # %% work with card_number 
+        # work with card_number 
         df['card_number'] = df['card_number'].astype('string')
 
-
-        # %% clean card_provider
+        # clean card_provider
         df['card_provider'] = df['card_provider'].astype('string')
-        #%%
         card_prov_ls = ['VISA 16 digit', 'JCB 16 digit', 'VISA 13 digit', 'JCB 15 digit', 'VISA 19 digit', 'Diners Club / Carte Blanche', 'American Express', 'Maestro', 'Discover', 'Mastercard']
-        #%%
         df['valid_provider'] = df['card_provider'].apply(lambda x: 'Yes' if x in card_prov_ls else 'No')
-        #%%
         df = df[df['valid_provider']=='Yes']
-        #%%
         df['card_provider'] = df['card_provider'].astype('category')
-        #%%
         df.drop(['valid_provider'], axis=1, inplace=True) 
 
-
-
-        # %% clean date_payment_confirmed
-        set(df['date_payment_confirmed'])
-        # %%
-        df['date_payment_confirmed'] # just transform into datetime
-        # %%
+        # clean date_payment_confirmed
         df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], errors='coerce', infer_datetime_format=True)
-        # %%
 
-
-        # %% work with expiry date
-        set(df['expiry_date']) #some values need to be dropped which are not dates but texts and occurs only once
-        # %%
-        df['expiry_date'].value_counts()
-        # %%
+        # work with expiry date
         df['dt'] = pd.to_datetime(df['expiry_date'], format="%m/%y")
-        #%%
-        df['dt']
-        # %%
         # df['expiry_date'] = df['dt'].map(lambda x: x.strftime('%m/%y'))
-        # %%
         df['expiry_date'] = pd.to_datetime(df['dt']).dt.to_period('M')
-        # %%
-        df['expiry_date']
-        # %%
         df.drop(['dt'], axis=1, inplace=True)
 
-
-
-        # %% working with Unnamed:0 
+        # working with Unnamed:0 
         df.rename(columns={'Unnamed: 0': 'groups'}, inplace=True)
         return df
     

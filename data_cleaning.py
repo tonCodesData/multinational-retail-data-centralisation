586e20c5482f3efe86101176d69d5c214a34d60e
Tonmoy <rashid920129@gmail.com>
2023-03-31 23:58:51 +0100

    revisits cleaning dates

--- data_cleaning.py
+++ data_cleaning.py
@@ -186,11 +186,7 @@ class DataCleaning:
 
     #m2t8 
     def clean_events_date(self, df):
-        df['date'] = df['year'] + '-' + df['month'] +'-' + df['day']
-        df['date_time'] = df['date'] + ' ' + df['timestamp']
-        df['timestamp'] = pd.to_datetime(df['date_time'], errors='coerce', infer_datetime_format=True)
-        df.drop(['year','month','date','day', 'date_time'], axis=1, inplace=True)
         df = df[df['time_period'].apply(lambda x: x in ['Evening', 'Midday', 'Morning', 'Late_Hours'])]
-        df['time_period'] = df['time_period'].astype('category')
         df['date_uuid'] = df['date_uuid'].astype('string')
+        df.info()
         return df
--- dates_cleaning.py
+++ dates_cleaning.py
@@ -8,17 +8,10 @@ pd.set_option('display.max_rows', 1000)
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.width', 1000)
 #%%
-df['date'] = df['year'] + '-' + df['month'] +'-' + df['day']
-#%%
-df['date_time'] = df['date'] + ' ' + df['timestamp']
-# %%
-df['timestamp'] = pd.to_datetime(df['date_time'], errors='coerce', infer_datetime_format=True)
-# %%
-df.drop(['year','month','date','day', 'date_time'], axis=1, inplace=True)
+df.info()
+
 # %%
 df = df[df['time_period'].apply(lambda x: x in ['Evening', 'Midday', 'Morning', 'Late_Hours'])]
-# %%
-df['time_period'] = df['time_period'].astype('category')
 # %%
 df['date_uuid'] = df['date_uuid'].astype('string')
 # %%

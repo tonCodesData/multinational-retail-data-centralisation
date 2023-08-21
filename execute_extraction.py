f5941e7387f50ad837691cca352c8b2f5562be0d
Rashid Tonmoy <124179394+tonCodesData@users.noreply.github.com>
2023-08-21 18:57:40 +0100

    Update execute_extraction.py

--- execute_extraction.py
+++ execute_extraction.py
@@ -33,21 +33,20 @@ db_connector.upload_to_db(card_cleaned, 'dim_card_details', db_connector.init_lo
 
 
 #------------------------------------------------------------------------------
-#%% extract from api and clean details of each store, and upload
+# API endpoints for store data
 header_dict = {'x-api-key':'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
 num_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
 retrieve_store_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
+
+#%% extract from api and clean details of each store, and upload
 number_of_stores = data_extractor.list_number_of_stores(num_stores_endpoint, header_dict)
 stores_df = data_extractor.retrieve_stores_data(retrieve_store_endpoint, number_of_stores, header_dict)
-# %%
 store_data_cleaned = data_cleaner.clean_store_data(stores_df)
-#%%
 db_connector.upload_to_db(store_data_cleaned, 'dim_store_details', db_connector.init_local_db_engine())
 
 
-#%% --------------------------------------------------------
-# M2Task6: Extract and clean product details
-
+#--------------------------------------------------------
+# Extract and clean product details
 # downloading products csv from s3 bucket
 s3_address = 's3://data-handling-public/products.csv'
 products_df = data_extractor.extract_from_s3(s3_address)
@@ -59,20 +58,19 @@ db_connector.upload_to_db(cleaned_prod_df, 'dim_products', db_connector.init_loc
 
 
 # %%--------------------------------------------------------
-# M2Task7: Retrieve and clean orders table from AWS RDS
-#%%step2: extract orders_table and convert into pd df
+# Retrieve and clean orders table from AWS RDS
+#%% extract orders_table and convert into pd df
 db_creds = db_connector.read_db_creds()
 engine = db_connector.init_db_engine(db_creds)
 orders_df = data_extractor.read_rds_table('orders_table', engine)
-#%%step3: clean the orders_df
+#%% clean the orders_df
 cleaned_orders_df = data_cleaner.clean_orders_data(orders_df)
 db_connector.upload_to_db(cleaned_orders_df, 'orders_table', db_connector.init_local_db_engine())
 
 # %%---------------------------------------------------------
-# M2task8: Retrieve and clean the date events JSON file from s3
+# Retrieve and clean the date events JSON file from s3
 s3_address = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json'
 date_events_df = data_extractor.extract_from_s3_zone(s3_address)
 
 cleaned_date_events_df = data_cleaner.clean_events_date(date_events_df)
 db_connector.upload_to_db(cleaned_date_events_df, 'dim_date_table', db_connector.init_local_db_engine())
-# %%

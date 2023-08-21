cb1361faf9fa0c842f8edee25cc825ade90c1077
Rashid Tonmoy <124179394+tonCodesData@users.noreply.github.com>
2023-08-21 18:12:18 +0100

    Update database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -3,23 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy import inspect
 import pandas as pd
 
+# this class is used to connect with and upload data to the database
 class DatabaseConnector:
-    # this class is used to connect with and upload data to the database
     def __init__(self) -> None:
         pass
 
-    # step2: Create a method read_db_creds 
-    # this will read the credentials yaml file 
-    # and return a dictionary of the credentials.
+    # read_db_creds() reads the credentials from yaml file and return a dictionary of the credentials
     def read_db_creds(self):
         with open('db_creds.yaml') as db_creds_file:
             db_creds = yaml.safe_load(db_creds_file)
         return db_creds
     
-    # step3: Now create a method init_db_engine 
-    # which will read the credentials 
-    # from the return of read_db_creds 
-    # and initialise and return an sqlalchemy database engine.
+    # init_db_engine() reads the dictionary credentials from the return of read_db_creds(), and initialises and returns an sqlalchemy database engine.
     def init_db_engine(self, db_creds):
         HOST = db_creds['RDS_HOST']
         PORT = db_creds['RDS_PORT']
@@ -31,14 +26,8 @@ class DatabaseConnector:
         engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
         return engine
 
-    #step 4: Using the engine from init_db_engine 
-    # create a method list_db_tables to list all the tables in db
-    # so you know which tables you can extract data from. 
-    # Develop a method inside your DataExtractor class 
-    # to read the data from the RDS database.
-
+    # list_db_tables() uses the engine from init_db_engine and lists all the tables in db
     def list_db_tables(self, engine):
-        # list all the tables
         ls_db_table_names = inspect(engine).get_table_names()
         return ls_db_table_names
     
@@ -53,9 +42,6 @@ class DatabaseConnector:
         engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
         return engine
 
-    #step 7: Now create a method in your DatabaseConnector 
-    # class called upload_to_db.
-    # This method will take in a Pandas DataFrame and table name 
-    # to upload to as an argument.
+    # upload_to_db() method takes in a Pandas DataFrame and table name to upload to database.
     def upload_to_db(self, df, table_name, engine):
         df.to_sql(name=table_name, con = engine, schema= 'public', if_exists='replace', index=False)
